6e2c4f3432023321d20ee66060b5e8c81199d183
LeeJongHyun <gus0000123@gmail.com>
2019-02-18 17:25:53 +0900

    first

---
+++ app/src/main/java/com/hyun/android/databindingsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hyun.android.databindingsample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
